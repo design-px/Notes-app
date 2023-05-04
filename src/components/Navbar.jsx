973b6ae56d3fb32c7f51cd14076befe293250317
design-px <you@example.com>
2023-05-04 14:18:22 +0530

    Adding light and dark theme feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+
+  const [themeMode, setThemeMode] = useState('')
+
+  useEffect(() => {
+    setThemeMode(localStorage.getItem('theme-mode') || 'dark')
+  }, [])
+
+  useEffect(() => {
+    document.querySelector('body').setAttribute('data-theme', themeMode)
+    localStorage.setItem('theme-mode', themeMode)
+  }, [themeMode])
+
+  const toggleTheme = () => {
+    setThemeMode(themeMode === 'dark' ? 'light' : 'dark')
+  }
 
   return (
     <>
@@ -8,6 +24,16 @@ export default function Navbar() {
 
         <NavLink to="/">Home</NavLink>
         <NavLink to="/notes">Notes</NavLink>
+
+        <div className="change-theme">
+          <input
+            type="checkbox"
+            className="theme-inp"
+            id="toggle-theme"
+            onChange={toggleTheme}
+          />
+          <label htmlFor="toggle-theme" className="theme-label"></label>
+        </div>
 
       </nav>
     </>
